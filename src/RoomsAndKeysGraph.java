a6915559e8100d99b93ec951768f1acf7aadacd4
Maria Sasinouskaya <maria.sasinouskaya@gmail.com>
2018-10-23 09:40:44 -0700

    --added description

--- src/RoomsAndKeysGraph.java
+++ src/RoomsAndKeysGraph.java
@@ -3,8 +3,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class RoomsAndKeysGraph {
-    /*There are N rooms and you start in room 0. Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
+/*There are N rooms and you start in room 0. Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
         Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.
         A key rooms[i][j] = v opens the room with number v.
         Initially, all the rooms start locked (except for room 0).
@@ -26,7 +25,7 @@ public class RoomsAndKeysGraph {
 
         Note: 1 <= rooms.length <= 1000 0 <= rooms[i].length <= 1000 The number of keys in all rooms combined is at most 3000.*/
 
-
+public class RoomsAndKeysGraph {
     public boolean canVisitAllRooms(List<List<Integer>> rooms){
         if(rooms.size() <= 1) return true;
         if(rooms.get(0).size() == 0) return false;
